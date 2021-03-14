2952ecf20b68f94beb55635f7567ebafa0342e70
Victor Nevola <victornevolapn@gmail.com>
2021-03-14 19:44:31 -0300

    Fixing bug in slider

--- components/Cards/index.tsx
+++ components/Cards/index.tsx
@@ -12,7 +12,7 @@ export const Cards = () => {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
+        // slidesToShow: 4,
         slidesToScroll: 2,
         variableWidth: true,
         arrows: false,
@@ -22,21 +22,21 @@ export const Cards = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    // slidesToShow: 3,
                     slidesToScroll: 3,
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 3,
+                    // slidesToShow: 3,
                     slidesToScroll: 3,
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 2,
+                    // slidesToShow: 2,
                     slidesToScroll: 2
                 }
             }
